1aa4a69e883ddf1bd14db8a5b0535348b51ac964
Ighnot <ighnot2002@yahoo.com>
2023-10-30 12:04:30 -0500

    First update with comments

--- src/ShapeMenu.java
+++ src/ShapeMenu.java
@@ -128,8 +128,10 @@ public class ShapeMenu {
                             + torus.calculateVolume());
                     break;
                 case "10":
-                    System.out.println("Thank you and goodbye!. Current date and " +
-                            "time: " + java.time.LocalDateTime.now());
+                    System.out.println("Thank you and goodbye!. Current time: " +
+                            java.time.LocalDateTime.now().format(java.time.format.
+                                    DateTimeFormatter.ofPattern("HH:mm:ss")));
+
                     break;
                 default:
                     System.out.println("Please use an integer from 1 to 10.");
@@ -146,7 +148,7 @@ public class ShapeMenu {
      * @return         A valid numeric value entered by the user.
      */
     private static double getValidInput(String prompt, Scanner scanner) {
-        double value = 0;
+        double value = -1; // Initialize with a negative value to ensure it's replaced with a valid input
         boolean validInput = false;
 
         while (!validInput) {
@@ -154,173 +156,16 @@ public class ShapeMenu {
             String input = scanner.nextLine();
             try {
                 value = Double.parseDouble(input);
-                validInput = true;
+                if (value >= 0) {
+                    validInput = true;
+                } else {
+                    System.out.println("Please enter a non-negative number.");
+                }
             } catch (NumberFormatException e) {
                 System.out.println("Bad input. Please use a number.");
             }
         }
-
         return value;
     }
 }
 
-
-class Shape {
-    private int numberOfDimensions;
-
-    public Shape(int numberOfDimensions) {
-        this.numberOfDimensions = numberOfDimensions;
-    }
-
-    public int getNumberOfDimensions() {
-        return numberOfDimensions;
-    }
-}
-
-class TwoDimensionalShape extends Shape {
-    private double area;
-
-    public TwoDimensionalShape(int numberOfDimensions, double area) {
-        super(numberOfDimensions);
-        this.area = area;
-    }
-
-    public double getArea() {
-        return area;
-    }
-}
-
-class ThreeDimensionalShape extends Shape {
-    private double volume;
-
-    public ThreeDimensionalShape(int numberOfDimensions, double volume) {
-        super(numberOfDimensions);
-        this.volume = volume;
-    }
-
-    public double getVolume() {
-        return volume;
-    }
-}
-
-class Circle extends TwoDimensionalShape {
-    private double radius;
-
-    public Circle(int numberOfDimensions, double radius) {
-        super(numberOfDimensions, Math.PI * radius * radius);
-        this.radius = radius;
-    }
-
-    public double calculateArea() {
-        return getArea();
-    }
-}
-
-class Rectangle extends TwoDimensionalShape {
-    private double length;
-    private double width;
-
-    public Rectangle(int numberOfDimensions, double length, double width) {
-        super(numberOfDimensions, length * width);
-        this.length = length;
-        this.width = width;
-    }
-
-    public double calculateArea() {
-        return getArea();
-    }
-}
-
-class Square extends Rectangle {
-    public Square(int numberOfDimensions, double sideLength) {
-        super(numberOfDimensions, sideLength, sideLength);
-    }
-}
-
-class Triangle extends TwoDimensionalShape {
-    private double baseLength;
-    private double height;
-
-    public Triangle(int numberOfDimensions, double baseLength, double height) {
-        super(numberOfDimensions, 0.5 * baseLength * height);
-        this.baseLength = baseLength;
-        this.height = height;
-    }
-
-    public double calculateArea() {
-        return getArea();
-    }
-}
-
-class Sphere extends ThreeDimensionalShape {
-    private double radius;
-
-    public Sphere(int numberOfDimensions, double radius) {
-        super(numberOfDimensions, (4.0 / 3.0) * Math.PI * Math.pow(radius, 3));
-        this.radius = radius;
-    }
-
-    public double calculateVolume() {
-        return getVolume();
-    }
-}
-
-class Cube extends ThreeDimensionalShape {
-    private double sideLength;
-
-    public Cube(int numberOfDimensions, double sideLength) {
-        super(numberOfDimensions, Math.pow(sideLength, 3));
-        this.sideLength = sideLength;
-    }
-
-    public double calculateVolume() {
-        return getVolume();
-    }
-}
-
-class Cone extends ThreeDimensionalShape {
-    private double radius;
-    private double height;
-
-    public Cone(int numberOfDimensions, double radius, double height) {
-        super(numberOfDimensions,
-                (1.0 / 3.0) * Math.PI * Math.pow(radius, 2) * height);
-        this.radius = radius;
-        this.height = height;
-    }
-
-    public double calculateVolume() {
-        return getVolume();
-    }
-}
-
-class Cylinder extends ThreeDimensionalShape {
-    private double radius;
-    private double height;
-
-    public Cylinder(int numberOfDimensions, double radius, double height) {
-        super(numberOfDimensions, Math.PI * Math.pow(radius, 2) * height);
-        this.radius = radius;
-        this.height = height;
-    }
-
-    public double calculateVolume() {
-        return getVolume();
-    }
-}
-
-class Torus extends ThreeDimensionalShape {
-    private double majorRadius;
-    private double minorRadius;
-
-    public Torus(int numberOfDimensions, double majorRadius, double minorRadius) {
-        super(numberOfDimensions,
-                2 * Math.PI * Math.PI * majorRadius * Math.pow(minorRadius, 2));
-        this.majorRadius = majorRadius;
-        this.minorRadius = minorRadius;
-    }
-
-    public double calculateVolume() {
-        return getVolume();
-    }
-}
